f5246e25e139465880a2bd86c24e001e55191892
Syn3ed <lol-uuu@bk.ru>
2023-11-07 10:00:23 +0700

    modified:   src/RequestUser/RequestUserDesc.js

--- src/RequestUser/RequestUserDesc.js
+++ src/RequestUser/RequestUserDesc.js
@@ -1,43 +1,46 @@
-import React,{ useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'; // Изменение импорта
 import RequestDescriptionForm from './RequestDescriptionForm';
 import { useTelegram } from "../Hooks/useTelegram";
 
 const requestList = [
-    { id: 1, subject: 'Тема 1', description: 'Описание 1', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
-    { id: 2, subject: 'Тема 2', description: 'Описание 2', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
-    { id: 3, subject: 'Тема 3', description: 'Описание 3', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
+  { id: 1, subject: 'Тема 1', description: 'Описание 1', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
+  { id: 2, subject: 'Тема 2', description: 'Описание 2', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
+  { id: 3, subject: 'Тема 3', description: 'Описание 3', dialog: 'Пользователь: *Существует*\nОператор: *Существует*' },
 ];
 
 const RequestUserDesc = () => {
-    const { tg, queryId } = useTelegram();
-    const { id } = useParams();
-    const request = requestList.find((item) => item.id === parseInt(id, 10));
-    const MainBut = () => {
-        tg.MainButton.hide();
-        tg.BackButton.show()
-    }
-    useEffect(() => {
-        const handleBackButton = () => {
-          history.goBack();
-        };
-        MainBut(); 
-        tg.BackButton.onClick(handleBackButton);
-        return () => {
-          tg.BackButton.offClick(handleBackButton);
-        };
-      }, [history]);
+  const { tg, queryId } = useTelegram();
+  const { id } = useParams();
+  const navigate = useNavigate(); // Изменение
 
+  const request = requestList.find((item) => item.id === parseInt(id, 10));
 
+  const MainBut = () => {
+    tg.MainButton.hide();
+    tg.BackButton.show()
+  }
 
-    if (!request) {
-        return <div>Заявка не найдена</div>;
-    }
-    return (
-        <div>
-            <RequestDescriptionForm request={request} />
-        </div>
-    );
+  useEffect(() => {
+    const handleBackButton = () => {
+      navigate(-1); // Изменение
+    };
+    MainBut();
+    tg.BackButton.onClick(handleBackButton);
+    return () => {
+    tg.BackButton.offClick(handleBackButton);
+    };
+  }, [navigate]);
+
+  if (!request) {
+    return <div>Заявка не найдена</div>;
+  }
+
+  return (
+    <div>
+      <RequestDescriptionForm request={request} />
+    </div>
+  );
 };
 
 export default RequestUserDesc;
